b90a6c129dd94d2ca60c23f2cfd786e56dd6913d
saket-prime <SAKETKUMAR0504@GMAIL.COM>
2023-12-09 04:49:33 +0530

    day0

---
+++ src/components/HeroBanner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Box, Typography, Button } from '@mui/material';
+
+const HeroBanner = () => {
+  return (
+    <Box>
+      <Typography>Fitness Club</Typography>
+      <Typography>Sweat, Smile <br/> and Repeat</Typography>
+    </Box>
+  )
+}
+
+export default HeroBanner
